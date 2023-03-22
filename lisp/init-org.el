bb95722b450b1d9804934b21f6a4201313c2eea3
arizalito <arizalito@protonmail.com>
2023-03-23 00:56:54 +0800

    Add org-agenda-files
    * lisp/init-org.el

--- lisp/init-org.el
+++ lisp/init-org.el
@@ -3,6 +3,7 @@
 (straight-use-package 'org-mode)
 (keymap-global-set "C-c a" #'org-agenda)
 (keymap-global-set "C-c c" #'org-capture)
+(setq org-agenda-files (directory-files-recursively "~/org/agenda/" "\\.org$"))
 
 (provide 'init-org)
 ;;; init-org.el ends here
