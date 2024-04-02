bc71d69cb88d50f0998957e1c696cba46de84c3e
arizalito <arizalito@protonmail.com>
2024-04-02 22:24:57 +0800

    Add new bindings of consult

--- lisp/init-package.el
+++ lisp/init-package.el
@@ -80,10 +80,21 @@
 
 ;; consult
 (leaf consult
-  :bind (("M-g g" . 'consult-goto-line)
-	 ("M-g M-g" . 'consult-goto-line)
-	 ("M-s l" . 'consult-line)
-	 ("C-x b" . 'consult-buffer)))
+  :bind
+  (("C-x M-:" . consult-complex-command)     ;; orig. repeat-complex-command
+   ("C-x b" . consult-buffer)                ;; orig. switch-to-buffer
+   ("C-x 4 b" . consult-buffer-other-window) ;; orig. switch-to-buffer-other-window
+   ("C-x 5 b" . consult-buffer-other-frame)  ;; orig. switch-to-buffer-other-frame
+   ("C-x t b" . consult-buffer-other-tab)    ;; orig. switch-to-buffer-other-tab
+   ("C-x r b" . consult-bookmark)            ;; orig. bookmark-jump
+   ("C-x p b" . consult-project-buffer)      ;; orig. project-switch-to-buffer
+   ("M-y" . consult-yank-pop)                ;; orig. yank-pop
+   ("M-g g" . consult-goto-line)             ;; orig. goto-line
+   ("M-g M-g" . consult-goto-line)           ;; orig. goto-line
+   ("M-g i" . consult-imenu)
+   ("M-g I" . consult-imenu-multi)
+   ("M-s l" . consult-line)
+   ("M-s L" . consult-line-multi)))
 
 ;; embark
 (leaf embark
